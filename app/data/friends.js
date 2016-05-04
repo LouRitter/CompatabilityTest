da4d99f17e203f118678ae7b3de4ee6eb34686f6
LouRitter <louritter1@gmail.com>
2016-05-04 11:32:42 -0400

    Needs more styling

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friendArr=[
+{
+  "name":"Lou",
+  "photoLink": "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/1496791_10208500699025296_5946231036009513778_n.jpg?oh=f3700a3d71dfd072fb51522d81ce56c9&oe=57AECD7D",
+  "scores":[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+     ]
+}
+];
+module.exports = friendArr; 
