277a22ae0fc3194b1e84a195b20a5fd6c409f13a
LouRitter <louritter1@gmail.com>
2016-05-04 12:53:40 -0400

    needs more styling

--- server.js
+++ server.js
@@ -3,15 +3,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
-// ==============================================================================
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server 
-// ==============================================================================
 
 var app = express(); // Tells node that we are creating an "express" server
 var PORT = process.env.PORT || 80; // Sets an initial port. We'll use this later in our listener
 
-// BodyParser makes it easy for our server to interpret data sent to it.
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
@@ -24,7 +20,7 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 // -------------------------------------------
 
 
-// The below code effectively "starts" our server 
+
 app.listen(PORT, function() {
 	console.log("App listening on PORT: " + PORT);
 });
